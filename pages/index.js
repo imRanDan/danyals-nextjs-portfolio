d0fb4894428817634ff2fb26b1c2f1cc97987d2c
Danyal Imran <dan.imran97@gmail.com>
2023-12-28 10:44:30 -0500

    added in navbar and footer components.

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import ContainerBlock from "../components/ContainerBlock";
 import Hero from "../components/Hero";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 
 
@@ -10,8 +12,9 @@ export default function Home() {
       title="Danyal Imran - Software Developer"
       description="Building websites since 2022. For more info please feel free to contact me!"
     >
+      <Navbar />
       <Hero />
-      
+      <Footer />
     </ContainerBlock>
   )
 }
