9288ba82ca25d09e6dd94113aa429840f8cc9592
Danyal Imran <dan.imran97@gmail.com>
2024-01-03 10:10:16 -0500

    working on styling contact page

--- components/Contact.js
+++ components/Contact.js
@@ -9,7 +9,7 @@ function Contact()  {
           Contact
         </h1>
       </div>
-      <div className="relative z-10 rounded-md shadow-md bg-[#02044A] p-4 md:p-10 lg:p-20 max-w-6xl mx-auto mb-20 -mt-4">
+      <div className="relative z-10 rounded-md shadow-md bg-[#a80041] p-4 md:p-10 lg:p-20 max-w-6xl mx-auto mb-20 -mt-4">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           <div className="md:ml-4">
             <header className="">
@@ -21,13 +21,13 @@ function Contact()  {
               </p>
             </header>
             <div className="icons-container inline-flex flex-col my-20">
-              <div className="flex flex-row items-center space-x-6 rounded-md border border-[#02044A] hover:border hover:border-blue-500 p-4">
+              <div className="flex flex-row items-center space-x-6 rounded-md border border-[white] hover:border hover:border-black p-4">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="16"
                   height="16"
                   fill="currentColor"
-                  className="bi bi-envelope-fill h-4 w-4 text-blue-500"
+                  className="bi bi-envelope-fill h-4 w-4 text-white hover:text-black"
                   viewBox="0 0 16 16"
                 >
                   <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555zM0 4.697v7.104l5.803-3.558L0 4.697zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757zm3.436-.586L16 11.801V4.697l-5.803 3.546z" />
@@ -108,8 +108,8 @@ function Contact()  {
               </a>
             </div>
           </div>
-          <form className="form rounded-lg bg-white p-4 flex flex-col">
-            <label htmlFor="name" className="text-sm text-gray-600 mx-4">
+          <form className="form rounded-lg bg-white p-4 flex flex-col dark:bg-gray-900">
+            <label htmlFor="name" className="text-sm text-gray-500 mx-4">
               {" "}
               Your Full Name
             </label>
@@ -118,7 +118,7 @@ function Contact()  {
               className="font-light rounded-md border focus:outline-none py-2 mt-2 px-1 mx-4 focus:ring-2 focus:border-none ring-blue-500"
               name="name"
             />
-            <label htmlFor="email" className="text-sm text-gray-600 mx-4 mt-4">
+            <label htmlFor="email" className="text-sm text-gray-500 mx-4 mt-4">
               Email
             </label>
             <input
@@ -128,7 +128,7 @@ function Contact()  {
             />
             <label
               htmlFor="message"
-              className="text-sm text-gray-600 mx-4 mt-4"
+              className="text-sm text-gray-500 mx-4 mt-4"
             >
               Message
             </label>
---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ContainerBlock from "@/components/ContainerBlock";
+import About from "@/components/About";
+
+function contact() {
+  return (
+    <ContainerBlock>
+      <About />
+    </ContainerBlock>
+  )
+}
+
+export default contact
