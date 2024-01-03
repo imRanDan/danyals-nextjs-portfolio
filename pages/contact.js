1532c0de553a63a0a0f76a0da012b62b46e47ac9
Danyal Imran <dan.imran97@gmail.com>
2024-01-02 19:07:12 -0500

    working on contact section of website

---
+++ pages/contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ContainerBlock from "@/components/ContainerBlock";
+import Contact from "@/components/Contact";
+
+function contact() {
+  return (
+    <ContainerBlock>
+      <Contact />
+    </ContainerBlock>
+  )
+}
+
+export default contact
