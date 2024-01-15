641a95cb00951fd45ba5134cd2ff84d26d90ccc2
Danyal Imran <dan.imran97@gmail.com>
2024-01-15 01:12:06 -0500

    changed latest code text to a more contrasted look/color when in dark mode

--- components/LatestCode.js
+++ components/LatestCode.js
@@ -8,7 +8,7 @@ export default function LatestCode({ repositories }) {
     <section className="bg-[#F1F1F1] -mt-20 dark:bg-gray-900 text-gray-500 pb-40">
       <div className="max-w-6xl mx-auto">
         <div className="flex flex-col md:flex-row justify-between items-center md:pt-40 mx-10">
-          <h1 className="text-6xl lg:text-9xl max-w-lg lg:py-3 lg:mb-16 font-bold text-gray-700 text-outline-100 py-20 md:my-0  dark:text-gray-600 text-center lg:text-left">
+          <h1 className="text-6xl lg:text-9xl max-w-lg lg:py-3 lg:mb-16 font-bold text-gray-700 text-outline-100 py-20 md:my-0  dark:text-gray-200 text-center lg:text-left">
             Latest Code
           </h1>
 
@@ -50,16 +50,16 @@ export default function LatestCode({ repositories }) {
 const GithubRepoCard = ({ getLatestRepos }) => {
   return (
     <div className="github-repo">
-      <h1 className="font-semibold text-xl dark:text-gray-200 text-gray-700">
+      <h1 className="font-semibold text-xl  text-gray-700 dark:text-white">
         {getLatestRepos.name}
       </h1>
-      <p className="text-base font-normal my-4 text-gray-700">
+      <p className="text-base font-normal my-4 text-gray-700 dark:text-white">
         {getLatestRepos.description}
       </p>
       <a
         target='_blank'
         href={getLatestRepos.clone_url}
-        className="font-semibold font-mono text-gray-700 group flex flex-row space-x-2 w-full items-center"
+        className="font-semibold font-mono text-gray-700 group flex flex-row space-x-2 w-full items-center dark:text-white"
       >
         <p>View Repository </p>
         <div className="transform   group-hover:translate-x-2 transition duration-300">
