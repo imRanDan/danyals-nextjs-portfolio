5256821663ebe8c4912565dcf4fe907741588eaf
Danyal Imran <dan.imran97@gmail.com>
2024-03-02 13:19:19 -0500

    Add vercel analytics to app

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,12 @@
 import "../styles/globals.css"
 import { ThemeProvider } from "next-themes"
-
+import { Analytics } from "@vercel/analytics/react"
 
 function _app({ Component, pageProps }) {
   return (
     <ThemeProvider defaultTheme="light" attribute="class">
       <Component {...pageProps} />
+      <Analytics />
     </ThemeProvider>
   )
 }
