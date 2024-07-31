53db2aaf80c6517cdd8782aa3c6d765d474edf2b
Danyal Imran <dan.imran97@gmail.com>
2024-07-30 23:00:56 -0400

    inquiry***

--- components/About.js
+++ components/About.js
@@ -35,7 +35,7 @@ function About() {
                   Contact
                 </h1>
                 <p className="text-lg text-gray-500 mt-4 dark:text-gray-300">
-                  For any sort of help / enquiry, shoot me an{" "}
+                  For any sort of help / inquiry, shoot me an{" "}
                   <a
                     href={`mailto:${userData.email}`}
                     className="text-gray-800 border-b-2 border-gray-800 dark:border-gray-300 font-bold dark:text-gray-300"
