6eb7bc944badaa60f8f05d7d1c88b23e6f72cd60
Danyal Imran <dan.imran97@gmail.com>
2024-11-26 11:08:57 -0500

    updated projects

--- constants/data.js
+++ constants/data.js
@@ -9,61 +9,53 @@ const userData = {
       title: "The Little Details",
       link: "https://thelittledetails.netlify.app/",
       imgUrl: "/the-little-details.png",
-      description: "A front-end design created for a home-staging client based out of Oakville, ON. They wanted a wireframe / mockup that showcases their work, who they are, what they do, and a contact form.",
-      stack: "HTML, CSS, JS, and SCSS."
-    },
-    {
-      title: "VroomVault",
-      link: "https://car-showcase-93id.vercel.app/",
-      imgUrl: "/Car-Showcase-App.png",
-      description:"A car dealership web app that generates information from a RapidAPI database and display cards with more info on car models with fuel and year model make.",
-      stack:"Built with TypeScript, Next.js, TailwindCSS, React, RapidAPI and HeadlessUI.",
+      description:
+        "A front-end design created for a home-staging client based out of Oakville, ON. They wanted a wireframe / mockup that showcases their work, who they are, what they do, and a contact form.",
+      stack: "HTML, CSS, JS, and SCSS.",
     },
     {
       title: "Ayesha's Hair Salon",
       link: "https://ayeshas-salon-responsive.netlify.app/",
       imgUrl: "/project-image-1.png",
-      description:"A mobile-first responsive website for a hair salon with information on barbers, services, lookbooks, contact information and more. ",
-      stack:"Built with HTML5, CSS3, and JavaScript.",
+      description:
+        "A mobile-first responsive website for a hair salon with information on barbers, services, lookbooks, contact information and more. ",
+      stack: "Built with HTML5, CSS3, and JavaScript.",
     },
     {
       title: "Password Generator",
       link: "https://the-cool-password-generator.netlify.app/",
       imgUrl: "/project-image-2.png",
-      description:"A mobile-first responsive website for generating passwords for accounts that you want to create or to update existing accounts.",
-      stack:"Built with HTML5, CSS3, and JavaScript.",
-    },
-    {
-      title: "TetrisJS",
-      link: "https://tetris-js-game.netlify.app/",
-      imgUrl: "/tetris-project.png",
-      description: "A JavaScript based game that I wanted to create for fun. Score is tracked and the game can be paused, reset, and comes with instructions. ",
-      stack:"Built with HTML5, CSS3, JavaScript, Blood, Sweat, and Tears.",
+      description:
+        "A mobile-first responsive website for generating passwords for accounts that you want to create or to update existing accounts.",
+      stack: "Built with HTML5, CSS3, and JavaScript.",
     },
     {
       title: "Color Explorer",
       link: "https://v49-tier2-team-17.netlify.app/",
       imgUrl: "/color-explorer-project.png",
-      description: "A collaborative project from Chingu, where a team of eight and I developed a color palette app. The app utilizes Grok AI to assist users in generating a color palette based on their input of a desired color scheme and a sample color.",
-      stack:"Built with HTML5, CSS3, JavaScript, Grok AI.",
+      description:
+        "A collaborative project from Chingu, where a team of eight and I developed a color palette app. The app utilizes Grok AI to assist users in generating a color palette based on their input of a desired color scheme and a sample color.",
+      stack: "Built with HTML5, CSS3, JavaScript, Grok AI.",
     },
     {
       title: "Imus Canada",
       link: "https://imus-canada.com/",
       imgUrl: "/imus-canada-client.png",
-      description: "A client wanted a website for their business, I suggested squarespace as a platform that has great SEO and allows an interactive web building experience that the client can access.",
-      stack:"Built with Squarespace.",
+      description:
+        "A client wanted a website for their business, I suggested squarespace as a platform that has great SEO and allows an interactive web building experience that the client can access.",
+      stack: "Built with Squarespace.",
     },
     {
-      title: "RememberThat",
-      link: "https://rememberthat-app.netlify.app/",
-      imgUrl: "/rememberthat-app.png",
-      description: "A mobile-first responsive website mockup of an app meant to help the user stay on task and which showcases features, pricing, and contact form.",
-      stack:"HTML5, CSS, SCSS, JavaScript.",
+      title: "Instagram Clone / Social Media app",
+      link: "https://social-media-app-imrandan.vercel.app/auth",
+      imgUrl: "/social-media-app.jpg",
+      description:
+        "A mobile-first responsive social media app that is a clone of Instagram. User can register with a google account or enter email/password. Users can also post, comment, and like posts.",
+      stack: "HTML5, CSS, SCSS, JavaScript.",
     },
   ],
   about: {
-    title:"Software Developer - ",
+    title: "Software Developer - ",
     description: [
       `I'm both self and community-taught software developer specializing in full-stack web development. Outside of coding, I love working out, tracking my lifts and macros, gaming, making playlists on Apple Music and Tidal, spending time with my dog, going for walks, and exploring cafes.`,
       `I'm always eager to connect with others, learn, and grow together. Whether you're looking for a new friend, co-worker, or collaborator, feel free to reach out!`,
@@ -75,6 +67,6 @@ const userData = {
     linkedin: "https://www.linkedin.com/in/danyalimran/",
     github: "https://github.com/imRanDan",
   },
-}
+};
 
-export default userData
+export default userData;
