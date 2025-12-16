5c87db562c082fe88c0dde8258950dff83c8f513
Dan Imran <dan.imran97@gmail.com>
2025-12-16 00:35:20 -0500

    update job title

--- components/Timeline.js
+++ components/Timeline.js
@@ -22,7 +22,7 @@ const experiences = [
   },
   {
     year: "Mar 2022 – Mar 2023",
-    title: "Frontend-Focused Developer – 100Devs Bootcamp, Remote",
+    title: "Frontend-Focused Developer – 100Devs, Remote",
     description:
       "Completed a 700+ hour intensive focused on building production-ready web applications, including frontend feature development, code reviews, and collaborative sprints.",
   },
